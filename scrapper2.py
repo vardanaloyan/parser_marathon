5a5ddd7909cbc454a499c0c071f7594facfec91d
Vardan <valoyan2@gmail.com>
2019-04-10 12:38:02 +0400

    asd

--- scrapper2.py
+++ scrapper2.py
@@ -6,7 +6,7 @@ import re
 import json
 import time
 import datetime
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+# urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 headers = {
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
                       '(KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36',
